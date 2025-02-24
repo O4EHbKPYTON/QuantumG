226bea01b850f7d8e240e92934584fb3b6dbcfec
iobda <raulichkas@inbox.ru>
2025-02-24 18:04:10 +0200

    Player controller and animations.

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,43 +1,28 @@
 extends CharacterBody2D
 
-const SPEED = 130.0
+const SPEED = 350.0
+const JUMP_VELOCITY = -500.0
 
-const JUMP_VELOCITY = -300.0
-
-@onready var animated_sprite = $AnimatedSprite2D
-
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var animated_sprite: AnimatedSprite2D = %AnimatedSprite2D
 
 
-func _physics_process(delta):
-  # Add the gravity.
-  if not is_on_floor():
-	velocity.y += gravity * delta
-
-  # Handle jump.
-  if Input.is_action_just_pressed("jump") and is_on_floor():
-	velocity.y = JUMP_VELOCITY
-
-  var direction = Input.get_axis("move_left", "move_right")
-  
-  if direction > 0:
-	animated_sprite.flip_h = false
-  elif direction < 0:
-	animated_sprite.flip_h = true
-  
-  if is_on_floor():
-	if direction == 0:
-	  animated_sprite.play("idle")
+func _physics_process(delta: float) -> void:
+	if not is_on_floor(): #гравитация
+		velocity.y += get_gravity().y * delta
+	if Input.is_action_just_pressed("JUMP") and is_on_floor(): #прыжок
+		velocity.y = JUMP_VELOCITY
+	var direction := Input.get_axis("MOVE_LEFT", "MOVE_RIGHT") #направление движения где 1 - право / -1 - лево
+	if direction > 0:
+		animated_sprite.flip_h = false
+	if direction < 0:
+		animated_sprite.flip_h = true
+	if is_on_floor(): 
+		if direction == 0:
+			animated_sprite.play("IDLE")
+		else:
+			animated_sprite.play("RUN")
+	if direction:
+		velocity.x = direction * SPEED
 	else:
-	  animated_sprite.play("run")
-  else:
-	animated_sprite.play("jump")
-  
-  if direction:
-	velocity.x = direction * SPEED
-  else:
-	velocity.x = move_toward(velocity.x, 0, SPEED)
-
-  move_and_slide()
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	move_and_slide()
