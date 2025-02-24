d115aa360cf2dafc95cb133d92e0b5813fc8eca0
O4EHbKPYTON <yan.matveyev@outlook.com>
2025-02-24 21:24:47 +0200

    added box scene

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,28 +1,31 @@
 extends CharacterBody2D
 
-const SPEED = 350.0
-const JUMP_VELOCITY = -500.0
+const SPEED: float = 425.0
+const JUMP_VELOCITY: float = -370.0
 
 @onready var animated_sprite: AnimatedSprite2D = %AnimatedSprite2D
 
-
 func _physics_process(delta: float) -> void:
-	if not is_on_floor(): #гравитация
+	if not is_on_floor():  # Гравитация
 		velocity.y += get_gravity().y * delta
-	if Input.is_action_just_pressed("JUMP") and is_on_floor(): #прыжок
+	if Input.is_action_just_pressed("JUMP") and is_on_floor():  # Прыжок
 		velocity.y = JUMP_VELOCITY
-	var direction := Input.get_axis("MOVE_LEFT", "MOVE_RIGHT") #направление движения где 1 - право / -1 - лево
+
+	var direction: float = Input.get_axis("MOVE_LEFT", "MOVE_RIGHT")  # Направление движения
 	if direction > 0:
 		animated_sprite.flip_h = false
 	if direction < 0:
 		animated_sprite.flip_h = true
+
 	if is_on_floor(): 
 		if direction == 0:
 			animated_sprite.play("IDLE")
 		else:
 			animated_sprite.play("RUN")
-	if direction:
+
+	if direction != 0:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
 	move_and_slide()
